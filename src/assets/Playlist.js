bfb2a7b37f8d5aefb56b74ae1cea7478183dcad2
JohnPaolo18 <Paolokahlil@gmail.com>
2024-12-11 00:14:13 -0700

    Added more  songs

--- src/assets/Playlist.js
+++ src/assets/Playlist.js
@@ -15,6 +15,41 @@ const playlist = [
     uri: require("../assets/LiveWhileWereYoung.mp3"),
   },
   // Add more songs as needed
+  {
+    title: "In The End",
+    artist: "Linkin Park",
+    uri: require("../assets/InTheEnd.mp3"),
+  },
+  {
+    title: "Maps",
+    artist: "Maroon 5",
+    uri: require("../assets/Maps.mp3"),
+  },
+  {
+    title: "Unwritten",
+    artist: "Natasha Bedingfield",
+    uri: require("../assets/Unwritten.mp3"),
+  },
+  {
+    title: "Sugar",
+    artist: "Maroon 5",
+    uri: require("../assets/Sugar.mp3"),
+  },
+  {
+    title: "Bring Me To Life",
+    artist: "Evanescence",
+    uri: require("../assets/BringMeToLife.mp3"),
+  },
+  {
+    title: "Cant Stop The Feeling",
+    artist: "Justin Timberlake",
+    uri: require("../assets/CantStopTheFeeling.mp3"),
+  },
+  {
+    title: "Beautiful Things",
+    artist: "Benson Boone",
+    uri: require("../assets/BeautifulThings.mp3"),
+  },
 ];
 
 export default playlist;
